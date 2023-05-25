fc31847d020b20b696135eb49b51f6084e134f18
Haka <76921756+Hakorr@users.noreply.github.com>
2023-05-25 15:00:20 +0300

    Update CommLink.js

--- CommLink.js
+++ CommLink.js
@@ -108,7 +108,7 @@ class CommLinkHandler {
     }
 
     registerSendCommand(name, obj) {
-        this.commands[name] = data => this.commLinkSend(obj?.sender || this.commlinkID , name, obj?.data || data);
+        this.commands[name] = data => this.commLinkSend(obj?.commlinkID || this.commlinkID , name, data || obj?.data);
     }
 
     registerListener(sender, commandHandler) {
